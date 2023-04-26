4dddbf973d4362a3a2c5922c261c083c8e6867dd
Den <hostden@yandex.ru>
2023-04-26 03:03:24 +0300

    Добавил стили в диалоги изменил роутинги, сделал рефакторинг профайла

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,30 +2,37 @@ import React from "react";
 import s from "./Dialogs.module.css"
 import { NavLink } from "react-router-dom";
 
+const DialogItems = (props) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) =>{
+    return(
+        <div className={s.message}>{props.message}</div>
+    )
+}
+
 const Dialogs = (props) => {
     return ( 
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
-                    <NavLink to="/dialogs/1">Dima</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/2">Sasha</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/3">Pety</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/4">Ivan</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/5">Tanya</NavLink>
-                </div>
+                <DialogItems name="Dima" id="1" />
+                <DialogItems name="Sasha" id="2" />
+                <DialogItems name="Petya" id="3" />
+                <DialogItems name="Ivan" id="4" />
+                <DialogItems name="Tanya" id="5" />
+                <DialogItems name="Lena" id="6" />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
-                <div className={s.message}>Pariatur, eligendi molestiae.</div>
-                <div className={s.message}> Exercitationem esse voluptatibus, modi porro minima magni dignissimos facere quas aliquam obcaecati, debitis eius ipsam animi ipsum harum possimus.</div>
+                <Message message="Lorem ipsum dolor sit, amet consectetur adipisicing elit."/>
+                <Message message="Exercitationem esse voluptatibus, modi porro minima magni dignissimos facere quas aliquam obcaecati, debitis eius ipsam animi ipsum harum possimus."/>
+                <Message message="Pariatur, eligendi molestiae."/>
+                <Message message="Hellow World!!"/>
             </div>
 
         </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
-  return <div className={s.content}>
-  <div>
-  <img src="https://f9.mirkvartir.me/journal/custom/2b713fd5-2ff8-4184-b9ed-d2faf61a48a1.jpg" alt="" />
+  return (
+   <div>
+    <ProfileInfo/>
+    <MyPosts />
   </div>
-
-  <div className={s.profile}>
-    <img src="./ava.webp" alt="" />
-    <p>Дмитрий Кадиновский</p>
-  </div>
-
-  <MyPosts />
-</div>
+  )
 }
 
 export default Profile;
